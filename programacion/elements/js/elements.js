e4a1a1531f1b66c522679e55768e6b40b4c33113
Cherapi <josed980506@gmail.com>
2024-02-04 21:17:02 -0500

    avance en el videojuego elements, agregué funciones a los botones

--- programacion/elements/js/elements.js
+++ programacion/elements/js/elements.js
@@ -1,18 +1,33 @@
+//variables globales
+
+let vidaJugador = 3;
+let vidaPc = 3;
+let ataqueJugador = ""
+let ataqueEnemigo = ""
+let resultado = ""
+
+
 //funciones
 function iniciarJuego(){
-    let b_mascota = document.getElementById('b-mascota')
-    b_mascota.addEventListener('click', seleccionarMascotaJugador)
-
+    let b_mascota = document.getElementById('b-mascota');
+    b_mascota.addEventListener('click', seleccionarMascotaJugador);
+    let b_reiniciar = document.getElementById('boton-reiniciar')
+    b_reiniciar.addEventListener('click', reiniciar)
 }
-
+function aleatorio (min,max) {
+    return Math.floor(Math.random()* (max - min + 1) + min)   
+}
+// SELECCION DE MASCOTAS DEL PC Y DEL JUGADOR
 function seleccionarMascotaJugador(){
 
-    let tiburon = document.getElementById('tiburon')
-    let topo = document.getElementById('topo')
-    let fenix = document.getElementById('fenix')
-    let tortuga = document.getElementById('tortuga')
-    let leon = document.getElementById('leon')
-    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
+    
+    let tiburon = document.getElementById('tiburon');
+    let topo = document.getElementById('topo');
+    let fenix = document.getElementById('fenix');
+    let tortuga = document.getElementById('tortuga');
+    let leon = document.getElementById('leon');
+    
     
     if(tiburon.checked){
         spanMascotaJugador.innerHTML = "tiburon"
@@ -27,29 +42,125 @@ function seleccionarMascotaJugador(){
     } else {
         alert("no has seleccionado aún")
     }
+    
+    seleccionarMascotaPc();
 
-    seleccionarMascotaPc()
+    let b_fuego = document.getElementById('b-fuego');
+    b_fuego.addEventListener('click', ataqueFuego);
+
+    let b_agua = document.getElementById('b-agua');
+    b_agua.addEventListener('click', ataqueAgua);
+    
+    let b_tierra = document.getElementById ('b-tierra');
+    b_tierra.addEventListener('click', ataqueTierra);
+    let b_mascota = document.getElementById('b-mascota');
+    b_mascota.disabled = true;
 }
 
 function seleccionarMascotaPc(){
 
-    let listaMascotas = ["tiburon", "topo", "fenix", "tortuga", "leon"]
-    let mascotaPc = Math.floor(Math.random() * listaMascotas.length)
-    let mascotaPcSeleccionada = listaMascotas[mascotaPc]
-    let spanMascotaPc = document.getElementById('mascota-pc')
+    let mascotaPc = aleatorio(1,5)
+    let spanMascotaPc = document.getElementById('mascota-pc');
 
 
-    if(mascotaPcSeleccionada == "tiburon"){
+    if(mascotaPc == 1){
         spanMascotaPc.innerHTML = "tiburon"
-    } else if (mascotaPcSeleccionada == "topo"){
+    } else if (mascotaPc == 2){
         spanMascotaPc.innerHTML = "topo"
-    } else if (mascotaPcSeleccionada == "fenix"){
+    } else if (mascotaPc == 3){
         spanMascotaPc.innerHTML = "fenix"
-    } else if (mascotaPcSeleccionada == "tortuga"){
+    } else if (mascotaPc == 4){
         spanMascotaPc.innerHTML = "tortuga"
-    } else if (mascotaPcSeleccionada == "leon"){
+    } else if (mascotaPc == 5){
         spanMascotaPc.innerHTML = "leon"
     } 
+    
 }
+
+// ATAQUES DEL JUGADOR Y DEL PC
+
+function ataqueFuego() {
+    ataqueJugador = "Fuego"
+    ataquePc()
+} 
+function ataqueAgua(){
+    ataqueJugador = "Agua" 
+    ataquePc()
+}
+function ataqueTierra(){    
+    ataqueJugador = "Tierra"
+    ataquePc()
+}
+
+function ataquePc(){
+
+    let eleccionPC = aleatorio(1,3) 
+
+        if(eleccionPC == 1){
+            ataqueEnemigo = "Fuego"
+        } else if(eleccionPC == 2){
+            ataqueEnemigo = "Agua"
+        } else if(eleccionPC == 3){
+            ataqueEnemigo = "Tierra"
+        }
+    marcador()
+    crearMensaje()
+}
+//RESULTADOS 
+
+function crearMensaje(){
+
+    let sectionMensajes = document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu mascota atacó con '+ ataqueJugador +', la mascota del enemigo atacó con '+ ataqueEnemigo+' tu ' + resultado +'.'
+    sectionMensajes.appendChild(parrafo)
+
+    if(vidaPc == 0){
+        mensajeFinal("¡QUE GRAN VICTORIA!")
+    } else if (vidaJugador == 0){
+        mensajeFinal("PERDISTE")
+    }
+}
+function mensajeFinal(resultadofinal){
+    let sectionMensajes = document.getElementById('mensajes')
+    let r_final = document.createElement('h1')
+    r_final.innerHTML = resultadofinal
+    sectionMensajes.appendChild(r_final)
+
+    let b_fuego = document.getElementById('b-fuego');
+    b_fuego.disabled = true;
+
+    let b_agua = document.getElementById('b-agua');
+    b_agua.disabled = true;
+    
+    let b_tierra = document.getElementById ('b-tierra');
+    b_tierra.disabled = true;
+
+}
+function marcador(){
+    if(ataqueJugador == ataqueEnemigo){
+        resultado = "EMPATE";
+    } else if((ataqueJugador == "Fuego"  && ataqueEnemigo == "Tierra") || (ataqueJugador == "Agua"  && ataqueEnemigo == "Fuego") || (ataqueJugador == "Tierra"  && ataqueEnemigo == "Agua")){
+        resultado = "GANASTE";
+        vidaPc --;  
+    } else {
+        resultado = "PERDISTE";
+        vidaJugador --;
+    }
+    let spanVidaPc = document.getElementById('vida-pc');
+    spanVidaPc.innerHTML = vidaPc
+    let spanVidaJugador = document.getElementById('vida-jugador');
+    spanVidaJugador.innerHTML = vidaJugador
+
+}
+function reiniciar(){
+    location.reload()
+}
+
+//limite del juego
+
+
+
+
 
 window.addEventListener('load', iniciarJuego)

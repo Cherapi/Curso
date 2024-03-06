f9ca8ce62797bc013934860f50b58f57ddae8da2
Cherapi <josed980506@gmail.com>
2024-03-05 21:23:24 -0500

    aplicando css y varias modificaciones en el código js y html

--- programacion/elements/js/elements.js
+++ programacion/elements/js/elements.js
@@ -31,7 +31,7 @@ function seleccionarMascotaJugador(){
     sectionMascotas.style.display = 'none'
 
     let sectionAtaque = document.getElementById('seleccionar-ataque')
-    sectionAtaque.style.display = 'block'
+    sectionAtaque.style.display = 'flex'
 
     let spanMascotaJugador = document.getElementById('mascota-jugador');
     
@@ -40,18 +40,39 @@ function seleccionarMascotaJugador(){
     let fenix = document.getElementById('fenix');
     let tortuga = document.getElementById('tortuga');
     let leon = document.getElementById('leon');
+    let tarjetaJugador = document.getElementById('tarjeta-jugador')
     
     
     if(tiburon.checked){
-        spanMascotaJugador.innerHTML = "tiburon"
+        spanMascotaJugador.innerHTML = "tiburon";
+        let imagenJugador = document.createElement('img');
+        imagenJugador.src = "./assets/tiburon.png";
+        imagenJugador.alt = "tiburon";
+        tarjetaJugador.appendChild(imagenJugador);
     } else if (topo.checked){
-        spanMascotaJugador.innerHTML = "topo"
+        spanMascotaJugador.innerHTML = "topo";
+        let imagenJugador = document.createElement('img');
+        imagenJugador.src = "./assets/topo.png";
+        imagenJugador.alt = "topo";
+        tarjetaJugador.appendChild(imagenJugador);
     } else if (fenix.checked){
-        spanMascotaJugador.innerHTML = "fenix"
+        spanMascotaJugador.innerHTML = "fenix";
+        let imagenJugador = document.createElement('img');
+        imagenJugador.src = "./assets/fenix.png";
+        imagenJugador.alt = "fenix";
+        tarjetaJugador.appendChild(imagenJugador);
     } else if (tortuga.checked){
         spanMascotaJugador.innerHTML = "tortuga"
+        let imagenJugador = document.createElement('img');
+        imagenJugador.src = "./assets/tortuga.png";
+        imagenJugador.alt = "tortuga";
+        tarjetaJugador.appendChild(imagenJugador);
     } else if (leon.checked){
         spanMascotaJugador.innerHTML = "leon"
+        let imagenJugador = document.createElement('img');
+        imagenJugador.src = "./assets/leon.png";
+        imagenJugador.alt = "leon";
+        tarjetaJugador.appendChild(imagenJugador);
     } else {
         alert("no has seleccionado aún")
         reiniciar()
@@ -75,18 +96,39 @@ function seleccionarMascotaPc(){
 
     let mascotaPc = aleatorio(1,5)
     let spanMascotaPc = document.getElementById('mascota-pc');
+    let tarjetaPC = document.getElementById('tarjeta-pc')
 
 
     if(mascotaPc == 1){
-        spanMascotaPc.innerHTML = "tiburon"
+        spanMascotaPc.innerHTML = "tiburon";
+        let imagenPc = document.createElement('img');
+        imagenPc.src = "./assets/tiburon.png";
+        imagenPc.alt = "tiburon";
+        tarjetaPC.appendChild(imagenPc);
     } else if (mascotaPc == 2){
-        spanMascotaPc.innerHTML = "topo"
+        spanMascotaPc.innerHTML = "topo";
+        let imagenPc = document.createElement('img');
+        imagenPc.src = "./assets/topo.png";
+        imagenPc.alt = "topo";
+        tarjetaPC.appendChild(imagenPc);
     } else if (mascotaPc == 3){
-        spanMascotaPc.innerHTML = "fenix"
+        spanMascotaPc.innerHTML = "fenix";
+        let imagenPc = document.createElement('img');
+        imagenPc.src = "./assets/fenix.png";
+        imagenPc.alt = "fenix";
+        tarjetaPC.appendChild(imagenPc);
     } else if (mascotaPc == 4){
         spanMascotaPc.innerHTML = "tortuga"
+        let imagenPc = document.createElement('img');
+        imagenPc.src = "./assets/tortuga.png";
+        imagenPc.alt = "tortuga";
+        tarjetaPC.appendChild(imagenPc);
     } else if (mascotaPc == 5){
         spanMascotaPc.innerHTML = "leon"
+        let imagenPc = document.createElement('img');
+        imagenPc.src = "./assets/leon.png";
+        imagenPc.alt = "leon";
+        tarjetaPC.appendChild(imagenPc);
     } 
     
 }
@@ -124,22 +166,38 @@ function ataquePc(){
 
 function crearMensaje(){
 
-    let sectionMensajes = document.getElementById('mensajes')
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML = 'Tu mascota atacó con '+ ataqueJugador +', la mascota del enemigo atacó con '+ ataqueEnemigo+' tu ' + resultado +'.'
-    sectionMensajes.appendChild(parrafo)
+    let sectionMensajes = document.getElementById('resultado')
+    let ataqueDeJugador = document.getElementById('ataque-jugador')
+    let ataqueDePc= document.getElementById('ataque-pc')
+
+    let nuevoAtaqueJugador = document.createElement('p')
+    let nuevoAtaquePc = document.createElement('p')
+
+    sectionMensajes.innerHTML = resultado
+    nuevoAtaqueJugador.innerHTML = ataqueJugador
+    nuevoAtaquePc.innerHTML = ataqueEnemigo
+
+    //let parrafo = document.createElement('p')
+    //parrafo.innerHTML = 'Tu mascota atacó con '+ ataqueJugador +', la mascota del enemigo atacó con '+ ataqueEnemigo+' tu ' + resultado +'.'
+
+    ataqueDeJugador.appendChild(nuevoAtaqueJugador)
+    ataqueDePc.appendChild(nuevoAtaquePc)
+
+    let sectionAtaque = document.getElementById('seleccionar-ataque');
 
     if(vidaPc == 0){
-        mensajeFinal("¡QUE GRAN VICTORIA!")
+        mensajeFinal("¡QUE GRAN VICTORIA!");
+        sectionAtaque.style.display = 'none';
     } else if (vidaJugador == 0){
-        mensajeFinal("PERDISTE")
+        mensajeFinal("DERROTA");
+        sectionAtaque.style.display = 'none';
     }
 }
 function mensajeFinal(resultadofinal){
-    let sectionMensajes = document.getElementById('mensajes')
+    let sectionMensaje = document.getElementById('reiniciar')
     let r_final = document.createElement('h1')
     r_final.innerHTML = resultadofinal
-    sectionMensajes.appendChild(r_final)
+    sectionMensaje.appendChild(r_final)
 
     let sectionReiniciar = document.getElementById('reiniciar')
     sectionReiniciar.style.display = 'block'
